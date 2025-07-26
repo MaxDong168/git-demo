15e132abbda6bb0dd22a3ef7971772ac25525b04
Max Dong <max.dong.168@gmail.com>
2025-07-26 16:23:21 +0800

    變更日期格式

--- 1.py
+++ 1.py
@@ -1,20 +1,15 @@
-
-
-import datatime as dt
-print("Hello, World!")
-print("Hello, World!")
-
 # coding: utf-8
 import requests
 import pandas as pd
+import datetime as dt
 import json
 url = "https://api.cnyes.com/media/api/v1/newslist/category/headline"#連結
 payload ={
     "page":9,
     "limit":30,
     "isCategoryHeadline":1,
-    "startAt":1752644946,
-    "endAt":1753508946   
+    "startAt":int((dt.datetime.today()- dt.timedelta(days = 11)).timestamp()),
+    "endAt":int(dt.datetime.today().timestamp())   
 } #參數
 res = requests.get(url, params = payload) #連線鉅亨網
 jd = json.loads(res.text) #解析JSON to 
@@ -24,4 +19,3 @@ df['link'] = df['newsId'].apply(lambda x: 'https://m.cnyes.com/news/id/' + str(x
 #print(df)
 df
 df.to_csv('news.csv',encoding ='utf-8-sig')
-
